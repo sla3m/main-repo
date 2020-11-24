3dc05a6d6d4a9838abad317d0ae5998670135662
sla3m <lukasz.wandulski@gmail.com>
2020-11-24 18:39:11 +0100

    Component1 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.css";
 
+import Component1 from "./components/Component1/Component1";
+
 export default function App() {
   return (
     <>
-      <Component />
+      <Component1 />
     </>
   );
 }
