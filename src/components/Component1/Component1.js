013b71d216ea3c086e63153a20285b2cf6e810b1
Łukasz Wandulski <lukasz.wandulski@monogo.pl>
2020-11-24 19:42:36 +0100

    Component1 example fix

--- src/components/Component1/Component1.js
+++ src/components/Component1/Component1.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const Component1 = () => (
   <div>
-    <p> Component1 content... </p>
+    <p> Component1 content...  </p>
+    <div> Dodane z main-repo</div>
   </div>
 );
 export default Component1;
